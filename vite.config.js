74f7c915b738bdbe1ef7b11e4c579f9a709b63bc
Samuel <samurrdp@gmail.com>
2025-12-19 14:12:49 -0500

    changed vite build

--- vite.config.js
+++ vite.config.js
@@ -17,16 +17,4 @@ export default defineConfig({
             },
         }),
     ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return 'vendor';
-                    }
-                }
-            }
-        },
-        chunkSizeWarningLimit: 1000,
-    }
 });
